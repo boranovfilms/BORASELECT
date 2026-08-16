79e54983f986992b23e8d92d8d82aeeb8ca7c009
boranovfilms <boranovfilms@gmail.com>
2026-08-16 13:43:55 -0300

    feat: adicionar rota /tarefa/:postId/:taskIndex para TarefaExecucao

--- src/App.tsx
+++ src/App.tsx
@@ -38,6 +38,7 @@ import MinhaDemandaDetalhe from './pages/MinhaDemandaDetalhe';
 import ProcessarPlanejamento from './pages/ProcessarPlanejamento';
 import Prospectar from './pages/Prospectar';
 import QuadroProducao from './pages/QuadroProducao';
+import TarefaExecucao from './pages/TarefaExecucao';
 import Orcamentos from './pages/orcamentos/Orcamentos';
 import OrcamentoDetalhe from './pages/orcamentos/OrcamentoDetalhe';
 import OrcamentoTemplates from './pages/orcamentos/OrcamentoTemplates';
@@ -154,6 +155,7 @@ export default function App() {
         <Route path="/tarefas" element={user ? wrapLayout(<Tarefas />) : <Navigate to="/login" />} />
         <Route path="/minhas-demandas" element={user ? wrapLayout(<MinhasDemandas />) : <Navigate to="/login" />} />
         <Route path="/producao" element={user ? wrapLayout(<QuadroProducao />) : <Navigate to="/login" />} />
+        <Route path="/tarefa/:postId/:taskIndex" element={user ? wrapLayout(<TarefaExecucao />) : <Navigate to="/login" />} />
         <Route path="/minha-demanda/:planId/:postId" element={user ? wrapLayout(<MinhaDemandaDetalhe />) : <Navigate to="/login" />} />
         <Route path="/teleprompter" element={user ? wrapLayout(<Teleprompter />) : <Navigate to="/login" />} />
         <Route path="/diagnostico" element={user && isAdmin ? wrapLayout(<DebugTable />) : <Navigate to="/" />} />
